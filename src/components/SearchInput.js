c9d90f8b3e67be626a9bfafd5d473e0863fde5a7
99-zziy <wldud8229@gmail.com>
2022-08-27 12:57:47 +0900

    :sparkles: input autofocus 설정, input click 하면 내용 삭제

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -6,14 +6,19 @@ class SearchInput {
     const $searchInput = document.createElement("input");
     this.$searchInput = $searchInput;
     this.$searchInput.placeholder = "고양이를 검색해보세요.|";
+    this.$searchInput.autofocus = true;
 
     $searchInput.className = "SearchInput";
     $target.appendChild($searchInput);
 
     $searchInput.addEventListener("keyup", (e) => {
-      if (e.keyCode === 13) {
+      if (e.key === "Enter") {
         onSearch(e.target.value);
       }
+    });
+
+    $searchInput.addEventListener("click", () => {
+      $searchInput.value = "";
     });
 
     console.log("SearchInput created.", this);
